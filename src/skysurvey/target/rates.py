Fix for the cosmology scaling and time dilation effect on the rate

src/skysurvey/target/rates.py
@@ -91,10 +91,10 @@ def get_rate(z, rate, **kwargs):
     if callable(rate): # function
         n_per_gpc3 = rate(z, **kwargs)
     else: # volumetric
-        n_per_gpc3 = np.full_like(z, rate)
+        n_per_gpc3 = np.full_like(z, rate, dtype=np.float64)
         
-    return n_per_gpc3
-    
+    return n_per_gpc3 / (1 + z)
+
 def get_ntargets_per_shell(zmax, rate, zmin=0, zstep=1e-5, cosmology=Planck18, astype="int", **kwargs):
     """ Get the total number of target expected in the given volume.
 
@@ -137,25 +137,23 @@ def get_ntargets_per_shell(zmax, rate, zmin=0, zstep=1e-5, cosmology=Planck18, a
     bins_of_redshift = np.arange(zmin, zmax, step=zstep) # [ndim]
 
     # this define the volume of the universe
-    volume = cosmology.comoving_volume( bins_of_redshift ).to("Gpc**3").value # len(input_z) (+ 1 if keepsize)
+    volume = cosmology.comoving_volume( bins_of_redshift ).to("Gpc**3").value
+    volume *= cosmology.h**3
     # and this the shell of universe. This is used to compute cases of non-constante rates.
     shell = np.diff(volume) # [ndim-1]
 
     # this are the effective redshift of the shells
-    bins_of_redshift_mid = np.mean([bins_of_redshift[1:], bins_of_redshift[:-1]], axis=0) # [ndim-1]
+    bins_of_redshift_mid = (bins_of_redshift[1:] + bins_of_redshift[:-1])/2 # [ndim-1]
 
     # so this is the rate computed at the effective redshift of the shell
     # it basically assumes the rate to be constant within one shell.
     n_per_gpc3_of_shell = get_rate(bins_of_redshift_mid, rate, **kwargs) # [ndim-1]
     # the total number of target per shell is the volumetric_rate_per_shell * the shell_volume
-    if np.ndim(n_per_gpc3_of_shell) == 0 or (np.ndim(n_per_gpc3_of_shell) == 1 and len(n_per_gpc3_of_shell) == len(shell)):
-        ntargets_per_shell = n_per_gpc3_of_shell * shell
-    else:
-        ntargets_per_shell = np.atleast_1d(n_per_gpc3_of_shell)[:, None] * shell
+    ntargets_per_shell = n_per_gpc3_of_shell * shell
     
     return bins_of_redshift_mid, ntargets_per_shell.astype(astype)
-    
-def get_ntargets(zmax, rate, zmin=0, cosmology=Planck18, zstep=1e-5, force_shell=False, astype="int", **kwargs):
+
+def get_ntargets(zmax, rate, zmin=0, cosmology=Planck18, zstep=1e-5, astype="int", **kwargs):
     """ Get the total number of target expected in the given volume.
 
     Parameters
@@ -189,24 +187,12 @@ def get_ntargets(zmax, rate, zmin=0, cosmology=Planck18, zstep=1e-5, force_shell
     ntargets: float, array
         number(s) of target.
     """
-    # function or forced, hence shell computation
-    if callable(rate) or force_shell: 
-        bins_of_redshift_mid, ntargets_per_shell = get_ntargets_per_shell(zmax, rate, 
-                                                                          zmin=zmin, zstep=zstep, 
-                                                                          cosmology=cosmology,
-                                                                          astype="float", # request astype comes at "return"
-                                                                          **kwargs)
-        ntargets = ntargets_per_shell.sum(axis=-1) # respects rate dimension
-
-    # simple constant volumetric rate, so "V(zmax)-V(zmin) * Constant"
-    else: 
-        volume_zmax = cosmology.comoving_volume( zmax ).to("Gpc**3").value
-        volume_zmin = cosmology.comoving_volume( zmin ).to("Gpc**3").value
-        rate = np.atleast_1d(rate)
-        if np.ndim(rate) == 1:
-            ntargets = (volume_zmax-volume_zmin) * rate
-        else:
-            ntargets = (volume_zmax-volume_zmin) * rate[:,None]
+    bins_of_redshift_mid, ntargets_per_shell = get_ntargets_per_shell(zmax, rate, 
+                                                                        zmin=zmin, zstep=zstep, 
+                                                                        cosmology=cosmology,
+                                                                        astype="float", # request astype comes at "return"
+                                                                        **kwargs)
+    ntargets = ntargets_per_shell.sum(axis=-1) # respects rate dimension
 
     # squeeze() will be [float] => float
     return ntargets.astype(astype).squeeze()